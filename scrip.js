b96a46bce019ab5a10e51e2ea602e06c7d2ac78e
tatogotsiridze <tatogotsiridze@gmail.com>
2026-01-22 19:58:45 +0400

    asdd

---
+++ scrip.js
@@ -0,0 +1,11 @@
+const qula = prompt("enter your score on test");
+
+alert(qula / 10); 
+
+if (qula > 9) { 
+    alert("a");
+} else if (qula > 5) {
+    alert("b");
+} else {    
+    alert("c");
+}
