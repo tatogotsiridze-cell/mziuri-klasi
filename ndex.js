e8fe52a02f73f4ae7c9464c6735a30097bf86ffd
tatogotsiridze <tatogotsiridze@gmail.com>
2026-04-02 15:58:03 +0400

    map

--- ndex.js
+++ ndex.js
@@ -1,11 +1,19 @@
+const numbers = [1, 2, 3, 4];
+
+function myMap(array, callback) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    result[i] = callback(array[i], i, array);
+  }
+
+  return result;
+}
+
+const doubled = myMap(numbers, function (num) {
+  return num * 2;
+});
+
+console.log(doubled);
 
 
-document.body.innerHTML = "<button>Add to cart</button>";
-
-let button = document.querySelector("button");
-
-button.onclick = function() {
-  
-  console.log("Adding to cart  " );
-};
-  
